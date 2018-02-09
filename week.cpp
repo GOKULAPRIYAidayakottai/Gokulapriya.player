ca9ac250d6133539494d6eb9d509d192b3fbf45e
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-02-09 15:14:47 +0530

    Create week.cpp

---
+++ week.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    
+    char s[20];
+  
+    cout<<"enter the string";
+    cin>>s;
+   switch(s[0])
+   {
+       case 'm':cout<<"working day";
+                break;
+       case 'w':cout<<"working day";
+                break;
+       case 'f':cout<<"working day";
+                break;
+       case 't':
+                break;        
+                
+   }
+   
+    return 0;
+}
