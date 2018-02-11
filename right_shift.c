36e22f060045fdfc9ff22acb7955eec2f30bca8c
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-02-11 09:58:30 +0530

    Create right_shift.c

---
+++ right_shift.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int n,k,i,a[10],t,j;
+   printf("enter the no of elements");
+   scanf("%d",&n);
+  printf("enter no of times you want to right shift");
+  scanf("%d",&k);
+   printf("enter array elements");
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(j=0;j<k;j++)
+   {
+   t=a[n-1];
+   for(i=n-2;i>=0;i--)
+   {
+       a[i+1]=a[i];
+   }
+   a[0]=t;
+   }
+    for(i=0;i<n;i++)
+   {
+       printf("%d",a[i]);
+   }
+   
+   
+    return 0;
+}
+
