b9ac39f7c11087819779d8f7809293ea772dbe41
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-02-13 15:56:05 +0530

    Create unique_element.c

---
+++ unique_element.c
@@ -0,0 +1,42 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+   int a[20],i,j,count=0,n;
+   printf("enter the number of element");
+   scanf("%d",&n);
+   printf("enter all the element");
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+           if(i==j)
+           {
+            continue;   
+           }
+           else
+           {
+               if(a[i]==a[j])
+               {
+                   count++;
+               }
+           }
+       }
+       if(count==0)
+       {
+           printf("%d",a[i]);
+       }
+       count=0;
+   }
+   
+    return 0;
+}
+
+
+
