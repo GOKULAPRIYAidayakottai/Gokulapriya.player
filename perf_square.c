861930693ba0f879b57820aed93a937d79dce94e
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-01 15:44:39 +0530

    Create perf_square.c

---
+++ perf_square.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n1,n2,i,pro=0,j;
+printf("enter the limit");
+scanf("%d%d",&n1,&n2);
+
+for(i=n1;i<=n2;i++)
+{
+    for(j=1;j<n2;j++)
+    {
+if(pow(j,2)==i)
+{
+pro++;
+break;
+}
+}
+}
+printf("number of perfect square in the given range = %d",pro);
+
+    return 0;
+}
