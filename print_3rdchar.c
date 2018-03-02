359a0a24218b3d118877cdf345f32af1f7c2e475
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 13:59:15 +0530

    Update print_3rdchar.c

--- print_3rdchar.c
+++ print_3rdchar.c
@@ -1,13 +1,17 @@
 
-#include <stdio.h>
 
+#include <stdio.h>
+#include<string.h>
 int main()
 {
     char s[20];
-int i,j;
+int i,j,l=0;
 
 printf("enter the string");
 scanf("%s",&s);
+l=strlen(s);
+if((l>1)&&(l<100000))
+{
 printf("%c",s[0]);
 j=strlen(s);
 for(i=1;i<j;i++)
@@ -16,6 +20,11 @@ if((i%3)==0)
 {
 printf("%c",s[i]);
 }
+}
+}
+else
+{
+    printf("input size must with in 1 to 100000");
 }
 
     return 0;
