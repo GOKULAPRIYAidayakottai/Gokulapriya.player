95b428d91ba3f0aef6534c7d574e13038c9e5537
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-11 10:33:18 +0530

    Update water_land.c

--- water_land.c
+++ water_land.c
@@ -5,7 +5,7 @@ int main()
    int a[5][5],i,j,n,c=0;
    printf("enter the array size : ");
    scanf("%d",&n);
-   printf("enter array elements ");
+   printf("enter array elements (0's and 1's only) ");
    for(i=0;i<n;i++)
    {
        for(j=0;j<n;j++)
